d30b9c3a32d894ae83a6ad98322ce9e5aef5737b
riyagarasangi <riyagarasangi@gmail.com>
2024-12-24 11:07:00 +0530

    Replace bcrypt with bcryptjs to avoid native module build issues

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
+
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
